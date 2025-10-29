bd9361c9cb66037a41a9c03ff930b480e3790350
Алина Приймак <alinapriymak@yandex.ru>
2025-10-29 09:39:39 +0300

    Вынесение фикстур в отдельный файл conftest

---
+++ conftest.py
@@ -0,0 +1,27 @@
+import pytest
+
+from main import BooksCollector
+
+
+# фикстура для создания экземпляра класса BooksCollector()
+@pytest.fixture
+def collector(self):
+    return BooksCollector()
+
+    
+# фикстура для создания экземпляра со списком книг 
+@pytest.fixture
+def collector_with_books(collector):
+    collector.add_new_book('Гордость и предубеждение и зомби')
+    collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    collector.add_new_book('Трое в лодке')
+    collector.add_new_book('Божественная комедия')
+    collector.add_new_book('Властелин колец')
+        
+    collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+    collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Детективы')
+    collector.set_book_genre('Трое в лодке', 'Комедии')
+    collector.set_book_genre('Божественная комедия', 'Комедии')
+    collector.set_book_genre('Властелин колец', 'Фантастика')
+        
+    return collector
